be4cd2d1d57ca558df0a868ba7a5291add37af07
Nicholas Søjnæs <nws.media.dk@gmail.com>
2022-06-14 23:14:15 +0200

    Website

---
+++ script.js
@@ -0,0 +1,47 @@
+let show = document.querySelector(".show");
+
+document.querySelector(".hover").addEventListener("click", showcontent)
+
+function showcontent(){
+    show.classList.toggle("hide")
+}
+
+let show2 = document.querySelector(".show2");
+
+document.querySelector(".hover2").addEventListener("click", showcontent2)
+
+function showcontent2(){
+    show2.classList.toggle("hide2")
+}
+
+let show3 = document.querySelector(".show3");
+
+document.querySelector(".hover3").addEventListener("click", showcontent3)
+
+function showcontent3(){
+    show3.classList.toggle("hide3")
+}
+
+let show4 = document.querySelector(".show4");
+
+document.querySelector(".hover4").addEventListener("click", showcontent4)
+
+function showcontent4(){
+    show4.classList.toggle("hide4")
+}
+
+let show5 = document.querySelector(".show5");
+
+document.querySelector(".hover5").addEventListener("click", showcontent5)
+
+function showcontent5(){
+    show5.classList.toggle("hide5")
+}
+
+let show6 = document.querySelector(".show6");
+
+document.querySelector(".hover6").addEventListener("click", showcontent6)
+
+function showcontent6(){
+    show6.classList.toggle("hide6")
+}
